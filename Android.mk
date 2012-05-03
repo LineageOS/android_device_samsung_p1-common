f0523ed01ea9810d934d08fc19fa7682299035c0
eaut <eaut@boehm.org>
2012-05-03 18:04:56 +0200

    modular init/ueventd config file generation logic
    This patch allows different products to share parts of their init.rc
    and ueventd.rc configuration.
    
    This is done by splitting the configuration into separate files
    together with a build time logic to assemble the target configuration.
    It was created to avoid configuration duplication for all devices in the
    P1000 family. For now it is disabled for the product p1c.
    
    The following variables are used to build the final .rc files:
    
    TARGET_INITRC_IMPORT
    adds "import lines" to the init.<device>.rc file at build time. The to
    be imported files have to be copied separately via PRODUCT_COPY_FILES.
    
    TARGET_INITRC_FILES
    defines the init.rc files that are joined together to create
    init.<device>.rc
    
    TARGET_UEVENTDRC_FILES
    defines the ueventd.rc src files that are joined together to create
    ueventd.<device>.rc
    
    Change-Id: I2785836445799620dfe89b08256f1030a5814f67

--- Android.mk
+++ Android.mk
@@ -1,3 +1,40 @@
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),s5pc110)
     include $(all-subdir-makefiles)
 endif
+
+ifneq ($(findstring $(strip $(TARGET_DEVICE)),p1 p1l p1n),)
+
+include $(CLEAR_VARS)
+
+ifneq ($(findstring $(strip $(TARGET_DEVICE)),p1l p1n),)
+  LOCAL_MACHINE_START := p1
+else
+  LOCAL_MACHINE_START := $(TARGET_DEVICE)
+endif
+
+# ----
+# generate init.<device>.rc
+# ----
+
+LOCAL_INIT_FILE := $(TARGET_ROOT_OUT)/init.$(LOCAL_MACHINE_START).rc
+
+$(LOCAL_INIT_FILE): $(TARGET_INITRC_FILES) $(foreach file,$(TARGET_INITRC_IMPORT), $(TARGET_ROOT_OUT)/$(file))
+	$(hide) echo > $@
+	$(hide) $(foreach file,$(TARGET_INITRC_IMPORT),echo "import $(file)" >> $@;)
+	$(hide) echo >> $@
+	$(hide) cat $(TARGET_INITRC_FILES) >> $@
+
+ALL_GENERATED_SOURCES += $(LOCAL_INIT_FILE)
+
+# ----
+# generate ueventd.<device>.rc
+# ----
+
+LOCAL_UEVENTD_FILE := $(TARGET_ROOT_OUT)/ueventd.$(LOCAL_MACHINE_START).rc
+
+$(LOCAL_UEVENTD_FILE): $(TARGET_UEVENTDRC_FILES)
+	$(hide) cat $^ > $@
+
+ALL_GENERATED_SOURCES += $(LOCAL_UEVENTD_FILE)
+
+endif
